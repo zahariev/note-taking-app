879983d6a62679bf866b01a5b55a5927e6f88e5f
zahariev <zaharievboyan@gmail.com>
2023-09-10 20:36:00 +0300

    fix: jest setup

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ export default {
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   "ts-jest": {
     tsconfig: "tsconfig.json",
   },
